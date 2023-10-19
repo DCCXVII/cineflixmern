d75841a2cffae8fb4553374f9eb1676038ba6033
elyass <elyass.inbox@gmail.com>
2023-10-19 11:47:52 -0700

    cineflix is a mern stack movies & series platform

--- server/util/SecretToken.js
+++ server/util/SecretToken.js
@@ -3,13 +3,13 @@ const jwt = require("jsonwebtoken");
 
 const createSecretToken = (res, id) => {
   const token = jwt.sign({ id }, process.env.TOKEN_KEY, {
-    expiresIn: '30d',
+    expiresIn: "30d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !=='development', 
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV !== "development", 
+    maxAge: 30 * 24 * 60 * 60 * 1000, // in milliseconds
   });
 };
 
